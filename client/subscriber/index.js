9ac0a324c0998d5516abe48714e04091cbbeed70
leo <937310988@qq.com>
2017-08-25 14:04:28 +0800

    add listen event list

--- client/subscriber/index.js
+++ client/subscriber/index.js
@@ -3,6 +3,16 @@ const Message = require('tcp-framework').Message;
 const assert = require('assert');
 
 module.exports = class extends TcpClient {
+
+	constructor(config, events) {
+		super(config);
+		this.events = events;
+	}
+
+    onConnected() {
+        this.send(new Message(Message.SIGN_DATA, Buffer.from(JSON.stringify(events), 'utf8')));
+	}
+
     onMessage(incomingMessage) {
     	try {
     		const {event, params} = JSON.parse(incomingMessage.payload.toString('utf8'));
--- server/subscriber/index.js
+++ server/subscriber/index.js
@@ -2,40 +2,55 @@ const TcpServer = require('tcp-framework').Server;
 const Message = require('tcp-framework').Message;
 const log4js = require('log4js');
 const assert = require('assert');
+const SocketManage = require('../lib/socket_manage');
 
 module.exports = class extends TcpServer {
-	constructor(options) {
-		super(options);
-		this._subscribers = new Set();
+    constructor(options) {
+        super(options);
         this._logger = log4js.getLogger('subscriber');
-	}
+        this.socketManage = new SocketManage();
+    }
 
     onStarted() {
-		this._logger.info(`subscriber server started at ${this._options.host}:${this._options.port}`);
-	}
+        this._logger.info(`subscriber server started at ${this._options.host}:${this._options.port}`);
+    }
 
-	onStopped() {
-		this._logger.info(`subscriber server stopped at ${this._options.host}:${this._options.port}`);
-	}
+    onStopped() {
+        this._logger.info(`subscriber server stopped at ${this._options.host}:${this._options.port}`);
+    }
 
-	onConnected(socket) {
-		this._subscribers.add(socket);
-		this._logger.info(`subscriber client(${socket.remoteAddress}:${socket.remotePort}) connected`);
-	}
+    onConnected(socket) {
+        this._logger.info(`subscriber client(${socket.remoteAddress}:${socket.remotePort}) connected`);
+    }
 
-	onClosed(socket) {
-		this._subscribers.delete(socket);
-		this._logger.info(`subscriber client(${socket.remoteAddress}:${socket.remotePort}) closed`);
-	}
+    onClosed(socket) {
+        this.socketManage.clean(socket);
+        this._logger.info(`subscriber client(${socket.remoteAddress}:${socket.remotePort}) closed`);
+    }
 
-	onError(socket, err) {
-		this._logger.info(`subscriber error occurred at client(${socket.remoteAddress}:${socket.remotePort}): ${err.stack}`);
-	}
+    onError(socket, err) {
+        this._logger.info(`subscriber error occurred at client(${socket.remoteAddress}:${socket.remotePort}): ${err.stack}`);
+    }
 
-	broadcast(event, params) {
-		for (let socket of this._subscribers) {
-			this._logger.debug(`sending event[${event}] to subscriber ${socket.remoteAddress}:${socket.remotePort}`);
-			this.send(socket, new Message(Message.SIGN_DATA, Buffer.from(JSON.stringify({event, params}), 'utf8')));
-		}
-	}
-}
+    onMessage(socket, incomingMessage) {
+        try {
+            let selectEvent = [...new Set(JSON.parse(incomingMessage.payload.toString('utf8')))];
+            this._logger.debug(`subscriber client(${socket.remoteAddress}:${socket.remotePort} select events[${selectEvent}]`);
+            this.socketManage.listen(socket, selectEvent);
+        }
+        catch (err) {
+            this._logger.error(err);
+        }
+    }
+
+    broadcast(event, params) {
+        if (this.socketManage.whoListenedThisEvent(event)) {
+            for (let socket of this.socketManage.whoListenedThisEvent(event)) {
+                this._logger.debug(`sending event[${event}] to subscriber ${socket.remoteAddress}:${socket.remotePort}`);
+                this.send(socket, new Message(Message.SIGN_DATA, Buffer.from(JSON.stringify({event, params}), 'utf8')));
+            }
+        }
+    }
+};
+
+
